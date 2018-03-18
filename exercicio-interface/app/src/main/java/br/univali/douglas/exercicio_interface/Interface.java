37cf37e7965bbcdb971199c2450654ddcaa4ef70
Douglas Michielin <douglasmichielin@edu.univali.br>
2018-03-18 13:13:49 -0300

    Arquivos iniciais

---
+++ exercicio-interface/app/src/main/java/br/univali/douglas/exercicio_interface/Interface.java
@@ -0,0 +1,13 @@
+package br.univali.douglas.exercicio_interface;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Interface extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_interface);
+    }
+}
