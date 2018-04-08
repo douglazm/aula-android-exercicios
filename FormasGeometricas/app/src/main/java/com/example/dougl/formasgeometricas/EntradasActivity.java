2839bb9c1cd5442dbdec8d18c72dd4e0f7c87726
Douglas Michielin <douglasmichielin@edu.univali.br>
2018-04-08 04:44:41 -0300

    Adicionado navegação entre as Activity.

--- FormasGeometricas/app/src/main/java/com/example/dougl/formasgeometricas/EntradasActivity.java
+++ FormasGeometricas/app/src/main/java/com/example/dougl/formasgeometricas/EntradasActivity.java
@@ -1,7 +1,9 @@
 package com.example.dougl.formasgeometricas;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class EntradasActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class EntradasActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entradas);
+    }
+
+    public void onClickResultado (View view) {
+        Intent it = new Intent(EntradasActivity.this, ResultadoActivity.class);
+        startActivity(it);
     }
 }
