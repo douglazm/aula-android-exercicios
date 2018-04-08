d232cdb4c05ec7cd54f2c26cbf94b6b86f1749da
Douglas Michielin <douglasmichielin@edu.univali.br>
2018-04-08 04:33:35 -0300

    Criado as activity com layout base.

---
+++ FormasGeometricas/app/src/main/java/com/example/dougl/formasgeometricas/EntradasActivity.java
@@ -0,0 +1,13 @@
+package com.example.dougl.formasgeometricas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EntradasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entradas);
+    }
+}
